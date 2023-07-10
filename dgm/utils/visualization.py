874f3d0eeaabc4d0c28367fa0967bd96f50e235d
omroot <oualid.missaoui@gmail.com>
2023-07-09 20:04:58 -0400

    add utils

---
+++ dgm/utils/visualization.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST # Training dataset
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in a uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
