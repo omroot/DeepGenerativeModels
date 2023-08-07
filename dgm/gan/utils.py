a7b7e07381447c99112bcc5798dca11488838caf
omroot <oualid.missaoui@gmail.com>
2023-08-07 09:21:20 -0400

    add utils file to gan implementations

---
+++ dgm/gan/utils.py
@@ -0,0 +1,37 @@
+
+import torch
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+
+
+
+def get_noise(n_samples, input_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, input_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        input_dim: the dimension of the input vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, input_dim, device=device)
+
+def combine_vectors(x, y):
+    '''
+    Function for combining two vectors with shapes (n_samples, ?) and (n_samples, ?).
+    Parameters:
+      x: (n_samples, ?) the first vector.
+        This will be the noise vector of shape (n_samples, z_dim).
+      y: (n_samples, ?) the second vector.
+        Once again, in this assignment this will be the one-hot class vector
+        with the shape (n_samples, n_classes).
+    '''
+    combined = torch.cat([x.float(), y.float()], 1)
+    return combined
+
+
